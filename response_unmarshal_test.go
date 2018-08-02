9549360dbbb3270310c7ab12e24b2ed022a2efce
dopin <dopinninja@gmail.com>
2018-08-02 13:46:24 +0900

    Support both new and old port format
    Arukas API will change their port format which is the same as Docker
    Compose settings.
    It will support the old format for a while for requesting - creating
    an app with services - but it will always respond with new format.
    So supporting both new and old port format, you don't need to be
    nervous when to change code.
    
    After Arukas API released the new format, the old format converting
    part will be redudant and will be fine to removed.

---
+++ response_unmarshal_test.go
@@ -0,0 +1,64 @@
+package arukas
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func assertPort(t *testing.T, port *Port, number int32, protocol string) {
+	if port.Protocol != protocol || port.Number != number {
+		t.Errorf("Expected %d/%s but got %d/%s", number, protocol, port.Number, port.Protocol)
+	}
+}
+
+func TestUnmsharlOldPortFormat(t *testing.T) {
+	responseBody := `
+		{
+			"data": {
+				"type": "services",
+				"id": "uuid-1",
+				"attributes": {
+					"image": "my-factorio",
+					"ports": [
+						{"protocol": "tcp", "number": 80},
+						{"protocol": "tcp", "number": 443},
+						{"protocol": "udp", "number": 34197}
+					]
+				}
+			}
+		}
+	`
+
+	service := new(ServiceData)
+	if err := json.Unmarshal([]byte(responseBody), service); err != nil {
+		t.Fatal("Failed to unmarshal old format port:", err)
+	}
+
+	assertPort(t, service.Ports()[0], 80, "tcp")
+	assertPort(t, service.Ports()[1], 443, "tcp")
+	assertPort(t, service.Ports()[2], 34197, "udp")
+}
+
+func TestUnmsharlNewPortFormat(t *testing.T) {
+	responseBody := `
+		{
+			"data": {
+				"type": "services",
+				"id": "uuid-1",
+				"attributes": {
+					"image": "my-factorio",
+					"ports": ["80", "443/tcp", "34197/udp"]
+				}
+			}
+		}
+	`
+
+	service := new(ServiceData)
+	if err := json.Unmarshal([]byte(responseBody), service); err != nil {
+		t.Fatal("Failed to unmarshal old format port:", err)
+	}
+
+	assertPort(t, service.Ports()[0], 80, "tcp")
+	assertPort(t, service.Ports()[1], 443, "tcp")
+	assertPort(t, service.Ports()[2], 34197, "udp")
+}
